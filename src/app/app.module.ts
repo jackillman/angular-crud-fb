349514fb1d1bcdd99b3a88f7b9d90e046d8d5c6d
jackillman <jackillman@gmail.com>
2018-03-12 00:01:02 +0200

    without delete
    without delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
+// import { AngularFirestore } from 'angularfire2/firestore';
+ import { AngularFireModule, FirebaseApp } from 'angularfire2';
+ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
 
+import {environment} from '../environments/environment';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+
+
+import {ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    FormsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyChkg1U2rEvPlf4mxvAvuJmAd3YtlX0ELc',
+    authDomain: 'fire1-d8902.firebaseapp.com',
+    databaseURL: 'https://fire1-d8902.firebaseio.com',
+    projectId: 'fire1-d8902',
+    storageBucket: 'fire1-d8902.appspot.com',
+    messagingSenderId: '949730475365'
+  }
 };
